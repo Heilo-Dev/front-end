277573f001707b5fd95cc3218c6902c2be8603c0
Hridoy <rakibhasanhridoy776@gmail.com>
2022-11-26 16:30:28 +0600

    student-info api end point added

--- components/student/dashboard/edit-profile/edit-profile.tsx
+++ components/student/dashboard/edit-profile/edit-profile.tsx
@@ -1,10 +1,18 @@
+import axios from "axios";
 import React from "react";
+import { APIEndpoints } from "../../../../data";
 import OtherInfo from "./other-info";
 import Sidebar from "./sidebar";
 
 type Props = {};
 
 const EditProfile = (props: Props) => {
+  // const url=process.env.apiUrl + APIEndpoints.student;
+
+  // axios
+  //     .get(url)
+  //     .then((data) =>console.log(data.data))
+
   return (
     <section
       className="grid  grid-cols-[375px_minmax(700px,_1fr)] gap-12 items-start w-fit mx-auto 
