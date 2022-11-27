934c0d02287a8a5d136a368335ebe7c04baea252
mohaiminul69 <mohaiminul69@gmail.com>
2022-11-27 22:11:25 +0600

    getting user data from get me API through apiSlice

---
+++ redux/slices/apiSlice.ts
@@ -0,0 +1,33 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { APIEndpoints } from "../../data";
+
+var token;
+const getUserToken = async () => {
+  token = await localStorage.getItem("heiloUserToken");
+};
+getUserToken();
+
+// Define a service using a base URL and expected endpoints
+export const userApi = createApi({
+  reducerPath: "userApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: process.env.apiUrl,
+    prepareHeaders: (headers) => {
+      // If we have a token set in state, let's assume that we should be passing it.
+      if (token) {
+        headers.set("authorization", `Bearer ${token}`);
+      }
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getUserInfo: builder.query({
+      query: () => APIEndpoints.getUserInfo,
+    }),
+  }),
+});
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
+export const { useGetUserInfoQuery } = userApi;
--- redux/store.ts
+++ redux/store.ts
@@ -1,10 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { userApi } from "./slices/apiSlice";
 import counterReducer from "./slices/counterSlice";
+import useReducer from "./slices/userSlice";
+import useApi from "./slices/apiSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    user: useReducer,
+    api: useApi,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(userApi.middleware),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
