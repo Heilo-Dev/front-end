90850254af72ffbbaa7ee1db83f6ff228702b8eb
mohaiminul69 <mohaiminul69@gmail.com>
2022-12-02 01:36:32 +0600

    student profile API integrated

--- redux/slices/apiSlice.ts
+++ redux/slices/apiSlice.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { APIEndpoints } from "../../data";
 
-var token;
+var token: string | null;
 const getUserToken = async () => {
   token = await localStorage.getItem("heiloUserToken");
 };
