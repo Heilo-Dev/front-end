712d97ed37b2b1dfdd3d8ea52ddb31fdedc2d659
programmerbanna <programmerbanna@gmail.com>
2023-01-09 23:23:05 +0600

    home page modified

--- src/pages/student/homepage.tsx
+++ src/pages/student/homepage.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import AvailableTutors from "../../components/student/homepage/tutor-section";
-import BannerSection from "../../components/student/homepage/banner-section";
 
-import { HomepageLayout } from "../../layouts";
+// @@ imports css @@ //
+import AvailableTutors from "@components/student/homepage/tutor-section";
+import BannerSection from "@components/student/homepage/banner-section";
+import { HomepageLayout } from "@layouts";
 
-type Props = {};
-
-const Homepage = (props: Props) => {
+// @@========================= imports css ======================@@ //
+const Homepage = () => {
   return (
     <div>
       <HomepageLayout>
