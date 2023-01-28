fc7cfbac0b7f72d062e4b9d3675f272811ea796d
programmerbanna <programmerbanna@gmail.com>
2023-01-28 22:16:50 +0600

    searc bar responsive resolved

--- components/student/dashboard/student-find-tutor/search-bar.tsx
+++ components/student/dashboard/student-find-tutor/search-bar.tsx
@@ -4,54 +4,74 @@ import Slider from "rc-slider";
 import { InputBox, RoundedCheckBox } from "../../../inputs";
 
 type Props = {
-  getGender: (e: any) => void;
-  getSubject: (e: any) => void;
+    getGender: (e: any) => void;
+    getSubject: (e: any) => void;
 };
 
 const SearchBar = (props: Props) => {
-  const { getGender, getSubject, searchOn }: any = props;
-  return (
-    <div className="my-8">
-      <div className="w-fit mx-auto mb-4 ">
-        <div className="tabBorder w-fit rounded-l-full px-6 relative mr-20">
-          <select defaultValue="subject" onChange={(e) => getSubject(e)} id="">
-            <option value="English">English</option>
-            <option value="Bangla">Bangla</option>
-            <option value="Ict">Ict</option>
-          </select>
-          <select name="subject" id="">
-            <option value="">topic</option>
-          </select>
-          <button className="btnPrimary h-[45px] w-32  rounded-bl-full rounded-r-full absolute right-[-105px] top-[-2px]">
-            search
-          </button>
+    const { getGender, getSubject, searchOn }: any = props;
+    return (
+        <div className="my-8">
+            <div className="w-fit mx-auto mb-4 ">
+                <div className="tabBorder w-fit rounded-l-full px-6 relative mr-20">
+                    <select
+                        defaultValue="subject"
+                        onChange={(e) => getSubject(e)}
+                        id=""
+                    >
+                        <option value="English">English</option>
+                        <option value="Bangla">Bangla</option>
+                        <option value="Ict">Ict</option>
+                    </select>
+                    <select name="subject" id="">
+                        <option className="" value="">
+                            topic
+                        </option>
+                    </select>
+                    <button className="btnPrimary h-[45px] w-32  rounded-bl-full rounded-r-full absolute right-[-105px] top-[-2px]">
+                        search
+                    </button>
+                </div>
+            </div>
+            <div className="flex justify-around items-center flex-col sm:flex-row  my-3">
+                <div className="flex justify-around items-center">
+                    <div>
+                        <RoundedCheckBox
+                            value="male"
+                            onChange={(e) => getGender(e)}
+                        />
+                        <label htmlFor="">male</label>
+                    </div>
+                    <div>
+                        <RoundedCheckBox
+                            value="female"
+                            onChange={(e) => getGender(e)}
+                        />
+                        <label htmlFor="">female</label>
+                    </div>
+                </div>
+                <div className="flex justify-between sm:justify-around items-center">
+                    <div className="tabBorder rounded-full sm:px-3 py-0 ">
+                        <select
+                            name=""
+                            id=""
+                            className="focus:outline-none border-none outline-none bg-transparent"
+                        >
+                            <option value="">availability</option>
+                        </select>
+                    </div>
+                    <div className="tabBorder px-1 rounded-lg ">
+                        <InputBox
+                            type="search"
+                            placeholder="Search by ID"
+                            className="m-0 focus:border-none border-1 border-xl border-none text-center"
+                            onChange={() => {}}
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-      <div className="flex justify-around items-center my-3">
-        <div>
-          <RoundedCheckBox value="male" onChange={(e) => getGender(e)} />
-          <label htmlFor="">male</label>
-        </div>
-        <div>
-          <RoundedCheckBox value="female" onChange={(e) => getGender(e)} />
-          <label htmlFor="">female</label>
-        </div>
-        <div className="tabBorder rounded-full px-3 py-0">
-          <select name="" id="" className="focus:outline-none">
-            <option value="">availability</option>
-          </select>
-        </div>
-        <div className="tabBorder px-1 rounded-lg">
-          <InputBox
-            type="search"
-            placeholder="Search by ID"
-            className="m-0 focus:border-none border-1 border-xl border-none text-center"
-            onChange={() => {}}
-          />
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default SearchBar;
